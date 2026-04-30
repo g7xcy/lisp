80efc7bc05cdd8d98e5008f6e73f7b834bc6f299
Frederica <g7xcy@outlook.com>
2026-04-30 14:52:20 +0800

    OI

--- built-ins.rkt
+++ built-ins.rkt
@@ -5,7 +5,7 @@
 (define (lisp-apply f args) (apply f args))
 (define (lisp-list . xs) xs)
 
-(define built-in-xs '(+ - * / car cdr cons < > not apply null? pair? number? symbol? list))
+(define built-in-xs     '(+ - * / car cdr cons < > not apply      null? pair? number? symbol? list))
 (define built-in-vs (list + - * / car cdr cons < > not lisp-apply null? pair? number? symbol? lisp-list))
 
 (define initial-env
--- repl.rkt
+++ repl.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require "reader.rkt" "eval.rkt" "built-ins.rkt")
+(require "reader.rkt" "eval.rkt" "built-ins.rkt" readline/readline)
 (provide repl)
 
 (define (pretty-print v)
@@ -13,28 +13,58 @@
           (eq? (car v) 'quote)
           (= (length v) 2))
      (string-append "'" (pretty-print (cadr v)))]
+    ;; closure
+    [(and (pair? v) (eq? (car v) 'Closure))
+     "<closure>"]
     ;; list
     [(list? v)
      (string-append
        "("
        (string-join (map pretty-print v) " ")
        ")")]
-    ;; closure
-    [(and (pair? v) (eq? (car v) 'Closure))
-     "<closure>"]
     [else (format "~a" v)]))
+
+(define (balance-state s)
+  (let loop ([chars (string->list s)] [n 0])
+    (cond
+      [(< n 0) 'error]
+      [(null? chars)
+       (cond
+         [(= n 0) 'ok]
+         [else 'incomplete])]
+      [(char=? (car chars) #\()
+       (loop (cdr chars) (add1 n))]
+      [(char=? (car chars) #\))
+       (loop (cdr chars) (sub1 n))]
+      [else
+       (loop (cdr chars) n)])))
+
+(define (read-multiline)
+  (let loop ([acc ""])
+    (define line (readline (if (string=? acc "") "λ> " "... ")))
+    (cond
+      [(eof-object? line) line]
+      [else
+       (define new-acc
+         (if (string=? acc "")
+             line
+             (string-append acc "\n" line)))
+       (case (balance-state new-acc)
+         [(ok) new-acc]
+         [(incomplete) (loop new-acc)]
+         [(error)
+          (displayln "Parse Error: unexpected ')'")
+          (loop "")])])))
 
 (define (repl)
   (displayln "OI!")
   (displayln "Type :quit to exit.")
   (let loop ([env initial-env])
-    (display "λ> ")
-    (flush-output)
-    (define input (read-line))
+    (define input (read-multiline))
     (cond
-      [(eof-object? input) (displayln "\n1551")]
+      [(eof-object? input) (displayln "\noiiai")]
       [(string=? input "") (loop env)]
-      [(string=? input ":quit") (displayln "1551")]
+      [(string=? input ":quit") (displayln "oiiai")]
       [else
        (with-handlers ([exn:fail?
                         (lambda (e)
